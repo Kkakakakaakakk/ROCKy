161ad6faf29032ad057d770bce051b6e2a1a6e84
Kkakakakaakakk <138270446+Kkakakakaakakk@users.noreply.github.com>
2023-07-01 13:21:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497078', 'me', true],
-  ['6281255369012', 'Ash', true], 
+  ['94760301352', 'me', true],
+  ['94760301352', '࿓ͥ🇦🇴🇴ͣ͟͞᭕O ʟ 𝙳:|රොකීᬽ᭄', true], 
   [''] 
 ] //Number of owners
 
@@ -50,7 +50,7 @@ global.APIKeys = { // APIKey Here
 global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
 global.premium = 'true'
 global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.author = '@࿓ͥ🇦🇴🇴ͣ͟͞᭕O ʟ 𝙳:|රොකීᬽ᭄' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
